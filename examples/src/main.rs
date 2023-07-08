cdbfde2519d52b562b6910bd9135ad351913a7c8
Ardika Rommy Sanjaya <rommy.sanjaya@iconpln.co.id>
2023-07-09 00:12:32 +0700

    Add license header

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -1,16 +1,38 @@
+// MIT License
+//
+// Copyright (c) 2023 Ardika Rommy Sanjaya
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 use std::{env, str::FromStr};
 
 use euid::EUID;
 
 fn help() {
-    println!("Usage: euid_toys [COMMAND] [ARG1] [ARG2]");
+    println!("Usage: euid-toys [COMMAND] [ARG1] [ARG2]");
     println!("Examples:");
-    println!("  euid_toys create");
-    println!("  euid_toys create_with_extension 123");
-    println!("  euid_toys create_batch 10");
-    println!("  euid_toys create_with_extension_batch 123 10");
-    println!("  euid_toys from 123897324");
-    println!("  euid_toys from_string C90FS3R3Z3J80V6BBZF0NM7SRKV");
+    println!("  euid-toys create");
+    println!("  euid-toys create_with_extension 123");
+    println!("  euid-toys create_batch 10");
+    println!("  euid-toys create_with_extension_batch 123 10");
+    println!("  euid-toys from 123897324");
+    println!("  euid-toys from_string C90FS3R3Z3J80V6BBZF0NM7SRKV");
 }
 
 fn main() {
